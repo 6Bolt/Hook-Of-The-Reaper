d18e8920befc28fdf7e67d82e48b4d87f6bed619
Darren <dbertanz@hotmail.com>
2025-02-04 16:09:53 -0700

    Fixed mistake with brackets

--- HookerEngine/HookCOMPort.cpp
+++ HookerEngine/HookCOMPort.cpp
@@ -43,37 +43,39 @@ void HookCOMPort::Connect(const quint8 &comPortNum, const QString &comPortName, 
 
     //Check if it is Already Open, if so, do nothing
     if(comPortOpen[comPortNum] == false)
-
-    p_ComPortArray[comPortNum] = new QSerialPort(comPortName);
-    p_ComPortArray[comPortNum]->setBaudRate ((QSerialPort::BaudRate) comPortBaud);
-    p_ComPortArray[comPortNum]->setDataBits ((QSerialPort::DataBits) comPortData);
-    p_ComPortArray[comPortNum]->setParity ((QSerialPort::Parity) comPortParity);
-    p_ComPortArray[comPortNum]->setStopBits ((QSerialPort::StopBits) comPortStop);
-    p_ComPortArray[comPortNum]->setFlowControl ((QSerialPort::FlowControl) comPortFlow);
-
-    QSerialPortInfo newPortInfo(comPortName);
-    p_ComPortArray[comPortNum]->setPort (newPortInfo);
-
-    if(!p_ComPortArray[comPortNum]->open(QIODevice::ReadWrite))
     {
-        //If Failed to Open COM Port
-        QSerialPort::SerialPortError portError = p_ComPortArray[comPortNum]->error();
-        QString critMessage = "Can not open the Serial COM Port: "+comPortName+" on Port: "+QString::number(comPortNum)+". Make sure a light gun or a COM device is on that port number. Serial Port Error: "+QString::number(portError);
-        emit ErrorMessage("Serial COM Port Error",critMessage);
-        //qDebug() << critMessage;
-        return;
+
+        p_ComPortArray[comPortNum] = new QSerialPort(comPortName);
+        p_ComPortArray[comPortNum]->setBaudRate ((QSerialPort::BaudRate) comPortBaud);
+        p_ComPortArray[comPortNum]->setDataBits ((QSerialPort::DataBits) comPortData);
+        p_ComPortArray[comPortNum]->setParity ((QSerialPort::Parity) comPortParity);
+        p_ComPortArray[comPortNum]->setStopBits ((QSerialPort::StopBits) comPortStop);
+        p_ComPortArray[comPortNum]->setFlowControl ((QSerialPort::FlowControl) comPortFlow);
+
+        QSerialPortInfo newPortInfo(comPortName);
+        p_ComPortArray[comPortNum]->setPort (newPortInfo);
+
+        if(!p_ComPortArray[comPortNum]->open(QIODevice::ReadWrite))
+        {
+            //If Failed to Open COM Port
+            QSerialPort::SerialPortError portError = p_ComPortArray[comPortNum]->error();
+            QString critMessage = "Can not open the Serial COM Port: "+comPortName+" on Port: "+QString::number(comPortNum)+". Make sure a light gun or a COM device is on that port number. Serial Port Error: "+QString::number(portError);
+            emit ErrorMessage("Serial COM Port Error",critMessage);
+            //qDebug() << critMessage;
+            return;
+        }
+        else
+        {
+            //If Open COM Port
+            comPortOpen[comPortNum] = true;
+
+            connect(p_ComPortArray[comPortNum],SIGNAL(readyRead()),this,SLOT(ReadData()));
+
+            numPortOpen++;
+            isPortOpen = true;
+        }
+
     }
-    else
-    {
-        //If Open COM Port
-        comPortOpen[comPortNum] = true;
-
-        connect(p_ComPortArray[comPortNum],SIGNAL(readyRead()),this,SLOT(ReadData()));
-
-        numPortOpen++;
-        isPortOpen = true;
-    }
-
     //qDebug() << "Done with Connecting Port";
 
 }
