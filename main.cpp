61812e09b04424b6646c53c9dfb59d7db49a8e5e
Darren <dbertanz@hotmail.com>
2025-02-17 19:17:40 -0700

    Fix bug to make only 1 HOTR open, so mutiple HOTR cannot be open

--- main.cpp
+++ main.cpp
@@ -2,14 +2,55 @@
 #include "hookOfTheReaper.h"
 
 #include <QApplication>
-
+#include <QSystemSemaphore>
+#include <QSharedMemory>
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    HookOfTheReaper w;
-    w.setWindowIcon (QIcon(":/data/icons/hOTRIcon256.ico"));
-    w.show();
-    return a.exec();
+
+
+    //Code to Check if Program Already Running or Not
+    //First Get Application Name
+    QString programName = QCoreApplication::applicationName();
+
+    //Make an Unique Program Name for Shared Memory and Semaphore
+    QString shardMemoryName = programName+"6969DEADBEEF";
+    QString semaphoreName = programName+"DEADBEEF6969";
+
+    //Create Semaphore with semaphoreName
+    QSystemSemaphore semaphore(semaphoreName, 1);
+
+    semaphore.acquire();
+
+    //Create Share Memory with shardMemoryName
+    QSharedMemory sharedMemory(shardMemoryName);
+    bool isRunning;
+
+    //If Memory can Be Created, then no other HOTR Running
+    //If false, then HOTR is Already Running
+    if (sharedMemory.create(1))
+        isRunning = false;
+    else
+        isRunning = true;
+
+    //Release semaphore before program Starts or Closes
+    semaphore.release();
+
+
+    //If Not Running, Start the Code, or Close if Running
+    if(!isRunning)
+    {
+        //If Not Running, then Get Going
+        HookOfTheReaper w;
+        w.setWindowIcon (QIcon(":/data/icons/hOTRIcon256.ico"));
+        w.show();
+        return a.exec();
+    }
+    else
+    {
+        //Close, if already running
+        return 1;
+    }
 }
